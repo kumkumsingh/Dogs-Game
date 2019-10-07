f7448c5e16956b9139f87e5631e749f53f4eb1b2
MelissaDTH <melissa.hart@ziggo.nl>
2019-10-07 15:34:14 +0200

    initial setup with reducer, store and provider

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+
+}
+
+const reducer = (state = [], action = {}) => {
+    switch (action.type) {
+    default:
+      return state
+    }
+  }
+  
+  export default reducer
+  
