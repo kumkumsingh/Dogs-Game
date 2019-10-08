a983b01a890b0ba3580aaf0d005ac0b8ab497d19
kumkumsingh <kumkumsingh.r@gmail.com>
2019-10-08 09:58:47 +0200

    have added action folder to use thunk

--- src/Components/DoglistContainer.js
+++ src/Components/DoglistContainer.js
@@ -5,20 +5,6 @@ import DogList from './DogList'
 export default class DoglistContainer extends Component {
   state = { dogBreeds: null }
   
-  componentDidMount() {
-    request
-      .get('https://dog.ceo/api/breeds/list/all')
-      .then(response => {
-        const breeds = Object.keys(response.body.message)
-        this.updateBreeds(breeds)
-      })
-      .catch(console.error)
-  }
-  updateBreeds(breeds) {
-    this.setState({
-      dogBreeds: breeds
-    })
-  }
   render() {
     return <DogList dogBreeds={this.state.dogBreeds} />
   }
