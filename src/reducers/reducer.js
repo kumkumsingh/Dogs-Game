747ef6c637d4685c72a78f06e13a636dfbfd3b8b
kumkumsingh <kumkumsingh.r@gmail.com>
2019-10-08 17:58:19 +0200

    added ...action.payload

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -5,7 +5,7 @@ const reducer = (state = [], action = {}) => {
     case 'SET_DOGS_LIST':
     // first we checked to see what the action.payload actually was with console.log, then we decided to only return this because this is the initial start. When we for example add something, then we have to return ...state.
         // console.log(action.payload);
-      return action.payload
+      return [...action.payload]
 
     default:
       return state
