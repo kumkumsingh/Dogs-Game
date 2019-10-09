c7bd85e7b2a693b543fbd4a610a7c13118ceb4c8
MelissaDTH <melissa.hart@ziggo.nl>
2019-10-09 10:11:57 +0200

    deleted the import of React since we are not using it

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
-
 const reducer = (state = [], action = {}) => {
     switch (action.type) {
     case 'SET_DOGS_LIST':
     // first we checked to see what the action.payload actually was with console.log, then we decided to only return this because this is the initial start. When we for example add something, then we have to return ...state.
         // console.log(action.payload);
-      return action.payload
+      return [...action.payload]
 
     default:
       return state
